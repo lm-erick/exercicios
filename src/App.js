92f8f2eec6162d1ec16c0a2b4b0aa6dc2f51426b
Erick <erick.luan.monteiro@gmail.com>
2020-05-23 16:21:59 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { View, StyleSheet } from 'react-native'
+import Simples from './componentes/Simples'
+import ParImpar from './componentes/ParImpar'
+
+export default class App extends Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <Simples texto='Flexibibellll'/>
+        <ParImpar numero={22}/>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  f20: {
+      fontSize: 40
+  }
+})
